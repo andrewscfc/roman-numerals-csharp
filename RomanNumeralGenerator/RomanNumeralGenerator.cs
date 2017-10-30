882aed9f15380f343d0c8805264a30c482217107
Andrew Bennett <andrew_scfc@yahoo.co.uk>
2017-10-30 23:45:33 +0000

    Working implementation

--- RomanNumeralGenerator/RomanNumeralGenerator.cs
+++ RomanNumeralGenerator/RomanNumeralGenerator.cs
@@ -1,12 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RomanNumerals
 {
     public class RomanNumeralGenerator : IRomanNumeralGenerator
     {
+        private Dictionary<int, string> _thousandPortions = new Dictionary<int, string>
+        {
+            {0, String.Empty},
+            {1, "M"},
+            {2, "MM"},
+            {3, "MMM"}
+        };
+        
+        private Dictionary<int, string> _hundredPortions = new Dictionary<int, string>
+        {
+            {0, String.Empty},
+            {1, "C"},
+            {2, "CC"},
+            {3, "CCC"},
+            {4, "CD"},
+            {5, "D"},
+            {6, "DC"},
+            {7, "DCC"},
+            {8, "DCCC"},
+            {9, "CM"},
+        };
+        
+        private Dictionary<int, string> _tenPortions = new Dictionary<int, string>
+        {
+            {0, String.Empty},
+            {1, "X"},
+            {2, "XX"},
+            {3, "XXX"},
+            {4, "XL"},
+            {5, "L"},
+            {6, "LX"},
+            {7, "LXX"},
+            {8, "LXXX"},
+            {9, "XC"},
+        };
+        
+        private Dictionary<int, string> _unitPortions = new Dictionary<int, string>
+        {
+            {0, String.Empty},
+            {1, "I"},
+            {2, "II"},
+            {3, "III"},
+            {4, "IV"},
+            {5, "V"},
+            {6, "VI"},
+            {7, "VII"},
+            {8, "VIII"},
+            {9, "IX"},
+        };
+        
         public string Generate(int number)
         {
-            throw new NotImplementedException();
+            var thousandPortion = ResolvePortion(1000, _thousandPortions, number);
+            var hundredPortion = ResolvePortion(100, _hundredPortions, number);
+            var tenPortion = ResolvePortion(10, _tenPortions, number);
+            var unitPortion = ResolvePortion(1, _unitPortions, number);
+
+            return $"{thousandPortion}{hundredPortion}{tenPortion}{unitPortion}";
         }
+
+        private static string ResolvePortion(int portionStepSize, Dictionary<int, string> portionStepMappings, int number)
+        {
+            var portionStep = (number / portionStepSize) % 10;
+            return portionStepMappings[portionStep];
+        }
+        
+       
     }
 }
--- RomanNumeralGeneratorTest/RomanNumeralGeneratorTests.cs
+++ RomanNumeralGeneratorTest/RomanNumeralGeneratorTests.cs
@@ -9,15 +9,16 @@ namespace RomanNumeralGeneratorTest
     public class RomanNumeralGeneratorTests
     {
         private RomanNumeralGenerator _testObj;
-        private const string TallyCase = "tally case";
 
         [SetUp]
         public void Setup()
         {
             _testObj = new RomanNumeralGenerator();
         }
-        
-        public static (int number, string expectedNumeral)[] Numbers1To10ValueSource = {
+
+        public static (int unitPortion, string numeralPortion)[] Units = new[]
+        {
+            (0, string.Empty),
             (1, "I"),
             (2, "II"),
             (3, "III"),
@@ -29,14 +30,9 @@ namespace RomanNumeralGeneratorTest
             (9, "IX")
         };
         
-        [Test]
-        public void Generate_Numbers1To9_ExpectedNumerals(
-            [ValueSource(nameof(Numbers1To10ValueSource))](int number, string expectedNumeral) valueToExpected)
+        public static (int tenPortion, string numeralPortion)[] Tens = new[]
         {
-            TestAlgorithm(valueToExpected);
-        }
-        
-        public static (int number, string expectedNumeral)[] TensValueSource = {
+            (0, string.Empty),
             (10, "X"),
             (20, "XX"),
             (30, "XXX"),
@@ -47,72 +43,53 @@ namespace RomanNumeralGeneratorTest
             (80, "LXXX"),
             (90, "XC")
         };
-        
-        [Test]
-        public void Generate_Tens_ExpectedNumerals(
-            [ValueSource(nameof(TensValueSource))](int number, string expectedNumeral) valueToExpected)
-        {
-            TestAlgorithm(valueToExpected);
-        }
-        
-        public static (int number, string expectedNumeral)[] HundredsValueSource = {
-            (100, "X"),
-            (200, "XX"),
-            (300, "XXX"),
-            (400, "XL"),
-            (500, "L"),
-            (600, "LX"),
-            (700, "LXX"),
-            (800, "LXXX"),
-            (900, "XC")
-        };
-        
-        [Test]
-        public void Generate_Hundreds_ExpectedNumerals(
-            [ValueSource(nameof(TensValueSource))](int number, string expectedNumeral) valueToExpected)
-        {
-            TestAlgorithm(valueToExpected);
-        }
-        
-        public static (int number, string expectedNumeral)[] ThousandsValueSource = {
-            (1000, "X"),
-            (2000, "XX"),
-            (3000, "XXX")
-        };
-        
-        [Test]
-        public void Generate_Thousands_ExpectedNumerals(
-            [ValueSource(nameof(ThousandsValueSource))](int number, string expectedNumeral) valueToExpected)
-        {
-            TestAlgorithm(valueToExpected);
-        }
-        
-        public static (int number, string expectedNumeral)[] SubtractivePatternsValueSource = {
-            (14, "XIV"),
-            (19, "XIX"),
-            (44, "XLIV"),
-            (49, "XLIL"),
-            (99, "XCIX"),
-            
-        };
-        
-        [Test]
-        public void Generate_SubtractivePatterns_ExpectedNumerals(
-            [ValueSource(nameof(SubtractivePatternsValueSource))](int number, string expectedNumeral) valueToExpected)
-        {
-            TestAlgorithm(valueToExpected);
-        }
-        
 
-        private void TestAlgorithm((int number, string expectedNumeral) valueToExpected)
+        public static (int hundredPortion, string numeralPortion)[] Hundreds = new[]
+        {
+            (0, string.Empty),
+            (100, "C"),
+            (200, "CC"),
+            (300, "CCC"),
+            (400, "CD"),
+            (500, "D"),
+            (600, "DC"),
+            (700, "DCC"),
+            (800, "DCCC"),
+            (900, "CM")
+        };
+        
+        public static (int thousandPortion, string numeralPortion)[] Thousands = new[]
+        {
+            (0, string.Empty),
+            (1000, "M"),
+            (2000, "MM"),
+            (3000, "MMM")
+        };
+        
+        [Test]
+        public void Test(
+            [ValueSource(nameof(Thousands))](int thousandPortion, string numeralPortion) thousandValues,
+            [ValueSource(nameof(Hundreds))](int hundredPortion, string numeralPortion) hundredValues,
+            [ValueSource(nameof(Tens))](int tenPortion, string numeralPortion) tensValues,
+            [ValueSource(nameof(Units))](int unitPortion, string numeralPortion) unitValues
+            )
         {
             //Arrange
+            var testValue = thousandValues.thousandPortion +
+                            hundredValues.hundredPortion +
+                            tensValues.tenPortion +
+                            unitValues.unitPortion;
 
+            var expectedValue = thousandValues.numeralPortion +
+                                hundredValues.numeralPortion +
+                                tensValues.numeralPortion +
+                                unitValues.numeralPortion;
+            
             //Act
-            var result = _testObj.Generate(valueToExpected.number);
-
+            var result = _testObj.Generate(testValue);
+            
             //Assert
-            result.Should().Be(valueToExpected.expectedNumeral);
+            result.Should().Be(expectedValue);
         }
     }
 }
